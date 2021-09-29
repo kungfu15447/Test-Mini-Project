b45af4f56c70f5cfe8135fd63875f63d5aecb375
Frederik Jensen <44639732+kungfu15447@users.noreply.github.com>
2021-09-29 10:46:46 +0200

    class for integration and unit test for customerscontroller

---
+++ HotelBooking.UnitTests/Controllers/CustomersControllerTest.cs
@@ -0,0 +1,24 @@
+﻿using HotelBooking.Application.Customers;
+using HotelBooking.WebApi.Controllers;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelBooking.UnitTests.Controllers
+{
+    public class CustomersControllerTest
+    {
+        private CustomersController _customerController;
+        private Mock<CustomerDomainService> _customerService;
+        public CustomersControllerTest()
+        {
+            _customerService = new Mock<CustomerDomainService>();
+            _customerController = new CustomersController(_customerService.Object);
+        }
+
+        public void 
+    }
+}
