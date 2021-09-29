e006c14ebbc84d498d54d2c0f0e8d17727ebac9a
kristianUrup <43743914+kristianUrup@users.noreply.github.com>
2021-09-29 11:20:10 +0200

    First test in RoomDomainServiceTests made

---
+++ HotelBooking.UnitTests/DomainServices/RoomDomainServiceTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HotelBooking.Application.Common.Facade;
+using HotelBooking.Application.Rooms;
+using HotelBooking.Core;
+using Moq;
+using Xunit;
+
+namespace HotelBooking.UnitTests.DomainServices
+{
+    public class RoomDomainServiceTests
+    {
+        private Mock<IRepository<Room>> _mockRoomRepo;
+        private RoomsDomainService _roomService;
+
+        public RoomDomainServiceTests()
+        {
+            _mockRoomRepo = new Mock<IRepository<Room>>();
+            _roomService = new RoomsDomainService(_mockRoomRepo.Object);
+        }
+
+        [Fact]
+        public void Get_GetAll_AllRoomsReturned()
+        {
+            // Arrange
+            _mockRoomRepo.Setup(r => r.GetAll()).Returns(new List<Room>
+            {
+                new Room{Id = 1, Description = "A"},
+                new Room{ Id = 2, Description = "B"}
+            });
+            
+            // Act
+            var rooms = _roomService.GetAll();
+
+            // Assert
+            Assert.True(rooms.Count() > 0);
+            _mockRoomRepo.Verify(r => r.GetAll(),Times.Once);
+            _mockRoomRepo.VerifyNoOtherCalls();
+        }
+    }
+}
